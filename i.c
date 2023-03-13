5d4ba42a6fc8f84b61ac5074f15f4cb0d8fbfd87
2202090106 <126403903+2202090106@users.noreply.github.com>
2023-03-13 23:32:16 +0530

    Add files via upload

--- i.c
+++ i.c
@@ -4,13 +4,19 @@ int main()
     int i, n ;
     printf("Enter the number :");
     scanf("%d", &n);
-if ((i%3) == 0){
-            printf("Fizz\t");
-        else if ((i%5) == 0)
+if ((i%15) == 0){
+            printf("FizzBuzz\t");
+        }
+        else if ((i%5) == 0){
             printf("Buzz\t");
+        }
 
-        else 
-            printf("%d\t", i);
-    }
-    return 0;
+        else if(i%3==0){
+             printf("Fizz\t");
+        }
+        else
+        {
+            printf("%d\t");
+        }
+return 0;
 }
